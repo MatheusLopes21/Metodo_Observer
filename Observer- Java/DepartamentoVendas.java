81197cc94adff9395609f6dc782343b47061d360
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-11-10 20:58:58 -0300

    Update DepartamentoVendas.java

--- Observer- Java/DepartamentoVendas.java
+++ Observer- Java/DepartamentoVendas.java
@@ -1,3 +1,7 @@
+/* Pratica 6 Observer Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/
+
 public class DepartamentoVendas implements Observer {
     @Override
     public void update(String mensagem) {
