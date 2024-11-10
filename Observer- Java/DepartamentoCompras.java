448107609f7bf6b440b896060398e34d4c97a310
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-11-10 20:58:17 -0300

    Update DepartamentoCompras.java

--- Observer- Java/DepartamentoCompras.java
+++ Observer- Java/DepartamentoCompras.java
@@ -1,3 +1,7 @@
+/* Pratica 6 Observer Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/
+
 public class DepartamentoCompras implements Observer {
 
     public void update(String mensagem) {
