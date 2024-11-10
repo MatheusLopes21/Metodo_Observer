134e6a0bc0258c8ffb081f7e4a0e7b56b5fa0054
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-11-10 20:58:37 -0300

    Update DepartamentoMarketing.java

--- Observer- Java/DepartamentoMarketing.java
+++ Observer- Java/DepartamentoMarketing.java
@@ -1,3 +1,8 @@
+/* Pratica 6 Observer Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/
+
+
 public class DepartamentoMarketing implements Observer {
     
     public void update(String mensagem) {
