c421cfe2bda015f6b86e7b7effde21503176161c
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-11-10 21:00:13 -0300

    Update Observer.java

--- Observer- Java/Observer.java
+++ Observer- Java/Observer.java
@@ -1,3 +1,8 @@
+/* Pratica 6 Observer Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/
+
+
 public interface Observer {
     void update(String mensagem);
 }
