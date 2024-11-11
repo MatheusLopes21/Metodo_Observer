900dab897f7e616fa5feea320f1dc6383bb6a513
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-11-10 21:00:32 -0300

    Update Principal.java

--- Observer- Java/Principal.java
+++ Observer- Java/Principal.java
@@ -1,3 +1,8 @@
+/* Pratica 6 Observer Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/
+
+
 public class Principal {
     public static void main(String[] args) {
        
